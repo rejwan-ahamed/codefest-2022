45fb28e8080adde43669a33b829c164bd850c0fd
rejwan-ahamed <rejwanahamed85@gmail.com>
2022-09-21 14:51:52 +0600

    final commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily: {
+        gs: "'General Sans', sans-serif",
+        cl: "'Clash Display', sans-serif",
+      }
+    },
+  },
+  plugins: [],
+}
